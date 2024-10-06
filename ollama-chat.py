5919d33c65a33c6450c9949de45a445ba70da34c
Shakhal Levinson <shakhal.l@taboola.com>
2024-10-06 13:30:41 +0300

    add dotenv

--- ollama-chat.py
+++ ollama-chat.py
@@ -4,9 +4,11 @@ from client.client_provider import ClientProvider
 from engine.engine_provider import EngineProvider
 from prompts import prompt
 from functions import tools
-
+import os
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     parser = argparse.ArgumentParser(description='Chatbot example')
     parser.add_argument('--logging', type=str, default='INFO', help='Logging level')
     args = parser.parse_args()
@@ -14,7 +16,7 @@ def main():
     # Configure logging
     logging.basicConfig(level=args.logging, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    client = ClientProvider.provide("ollama", 'llama3.2:latest', tools, OLLAMA_API_KEY, None)
+    client = ClientProvider.provide("ollama", 'llama3.2:latest', tools, os.getenv('OLLAMA_API_KEY'), None)
     engine = EngineProvider.provide("ollama", client, prompt)
     engine.run()
 
